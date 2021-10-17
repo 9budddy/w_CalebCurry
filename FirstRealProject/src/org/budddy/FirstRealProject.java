4ec41e51ad34a9d4f8ef824f9681a23e163d6e08
Budddy <budddyjellz@gmail.com>
2021-10-17 17:21:32 -0400

    Iterator & ForEach Usage

--- FirstRealProject/src/org/budddy/FirstRealProject.java
+++ FirstRealProject/src/org/budddy/FirstRealProject.java
@@ -4,21 +4,29 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class FirstRealProject {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) {
+		
 		LinkedList<String> names = new LinkedList<String>();
 		
 		names.push("Caleb");
 		names.push("Sue");
 		names.push("Sally");
 		
-		System.out.println(names.remove());
-		System.out.println(names.remove());
-		System.out.println(names.remove());
+		Iterator<String> it = names.iterator();
+		
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+		
+		for(String name: names) {
+			System.out.println(name);
+		}
 	}
 }
