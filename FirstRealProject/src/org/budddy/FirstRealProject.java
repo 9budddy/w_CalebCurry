b744af657792df14d70f6329fdef2037481a20d8
Budddy <budddyjellz@gmail.com>
2021-10-17 14:05:46 -0400

    Console Scanner. Take Size, Numbers of Size, Output Numbers.

--- FirstRealProject/src/org/budddy/FirstRealProject.java
+++ FirstRealProject/src/org/budddy/FirstRealProject.java
@@ -5,19 +5,22 @@ import java.util.Scanner;
 public class FirstRealProject {
 
 	public static void main(String[] args) {
-		System.out.println("This is My First Real Project!");
 		
 		Scanner scan = new Scanner(System.in);
-		//String s = scan.nextLine();
-		
+		System.out.println("Give us a size.");
+		int size = scan.nextInt();
 
-		int i = 0;
-		do {
-			System.out.println("i is: " + i);
-			i++;
-		} while(i < 10);
+		int[] grades = new int[size];
 
+		System.out.println("Enter " + size + " numbers. Press enter after each.");
+		for(int i = 0; i < size; i++) {
+			grades[i] = scan.nextInt();
+		}
 		
 		scan.close();
+		
+		for(int i = 0; i < size; i++) {
+			System.out.println(grades[i]);
+		}
 	}
 }
