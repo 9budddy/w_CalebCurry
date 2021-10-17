77bf28353307ad693d1a1d85846f8fa69fbe60a0
Budddy <budddyjellz@gmail.com>
2021-10-17 15:07:35 -0400

    Use LinkedList as Queue

--- FirstRealProject/src/org/budddy/FirstRealProject.java
+++ FirstRealProject/src/org/budddy/FirstRealProject.java
@@ -3,30 +3,22 @@ package org.budddy;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class FirstRealProject {
 
-	List<String> stuff;
-	
 	public static void main(String[] args) throws FileNotFoundException {
+		LinkedList<String> names = new LinkedList<String>();
 		
-		Scanner scan = new Scanner(new File("Resources/Students.txt"));
+		names.add("Caleb");
+		names.add("Sue");
+		names.add("Sally");
 		
-		List<String> students = new ArrayList<String>();
-		
-		while (scan.hasNextLine()) {
-			students.add(scan.nextLine());
-		}
-		
-		students.forEach(student -> {
-			System.out.println("Name: " + student);
-		});
-		
-		scan.close();
-		
-		FirstRealProject p = new FirstRealProject();
-		p.stuff = new ArrayList<String>();
+		System.out.println(names.remove());
+		System.out.println(names.remove());
+		System.out.println(names.remove());
 	}
 }
