5025c46c51bab68800f53c6c60fbee17ab6d4b84
Budddy <budddyjellz@gmail.com>
2021-11-05 14:45:43 -0400

    HashSets, HashMaps

--- Collections/src/org/budddy/Collections.java
+++ Collections/src/org/budddy/Collections.java
@@ -1,8 +1,14 @@
 package org.budddy;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Collections {
 
 	public static void main(String[] args) {
+		HashSet<Person> people = new HashSet<Person>();
+
+		
 		Person p = new Person("email@email.com", "Jones");
 		Position pos1 = new Position(10,10);
 		p.position = pos1;
@@ -10,9 +16,13 @@ public class Collections {
 		Position pos2 = new Position(10,10);
 		q.position = pos2;
 		
-		System.out.println(p.equals(q));
-		System.out.println(p.hashCode());
-		System.out.println(q.hashCode());
-
+		people.add(p);
+		people.add(q);
+		
+		System.out.println(people.contains(p));
+		
+		HashMap<String, Person> peeps = new HashMap<String, Person>();
+		peeps.put("email@email.com", p);
+		System.out.println("HashMap: " + peeps.containsKey("email@email.com"));
 	}
 }
