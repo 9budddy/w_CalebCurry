8bb754a2b4c9281f74f693dd1d194030d84904f3
Budddy <budddyjellz@gmail.com>
2021-11-05 14:31:20 -0400

    Custom Class Objects (HashCode, Equals) Override

--- Collections/src/org/budddy/Collections.java
+++ Collections/src/org/budddy/Collections.java
@@ -1,28 +1,18 @@
 package org.budddy;
 
-import java.util.HashMap;
-import java.util.Map.Entry;
-import java.util.Set;
-
 public class Collections {
 
 	public static void main(String[] args) {
-		HashMap<String, Integer> ids = new HashMap<String, Integer>();
-		ids.put("Buddy Jones", 5);
-		ids.put("Caleb Curry", 345);
+		Person p = new Person("email@email.com", "Jones");
+		Position pos1 = new Position(10,10);
+		p.position = pos1;
+		Person q = new Person("email@email.com", "Jones");
+		Position pos2 = new Position(10,10);
+		q.position = pos2;
 		
-		ids.put("Caleb Curry", ids.getOrDefault("Caleb Curry", 0) + 1);
-		
-		for(Entry<String, Integer> id : ids.entrySet()) {
-			System.out.println(id.getKey());
-			System.out.println(id.getValue());
-		}
-		
-		Set<String> keys = ids.keySet();
-		for(String key : keys) {
-			System.out.println(key);
-			System.out.println(ids.get(key));
-		}
-		
+		System.out.println(p.equals(q));
+		System.out.println(p.hashCode());
+		System.out.println(q.hashCode());
+
 	}
 }
---
+++ Collections/src/org/budddy/Person.java
@@ -0,0 +1,35 @@
+package org.budddy;
+
+import java.util.Objects;
+
+public class Person {
+	String email;
+	String lastName;
+	Position position;
+	
+	Person(String email, String lastName) {
+		this.email = email;
+		this.lastName = lastName;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, lastName, position);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return (Objects.equals(lastName, other.lastName) &&
+				Objects.equals(email, other.email) &&
+				Objects.equals(position, other.position));
+	}	
+	
+	
+}
