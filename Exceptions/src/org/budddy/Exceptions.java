f973975a96daebe440d18be102641ff013ccef35
Budddy <budddyjellz@gmail.com>
2021-11-06 13:11:59 -0400

    Exception Throwing

---
+++ Exceptions/src/org/budddy/Exceptions.java
@@ -0,0 +1,25 @@
+package org.budddy;
+
+import java.io.IOException;
+
+public class Exceptions {
+
+	public static void main(String[] args) {
+		Exceptions exceptions = new Exceptions();
+		exceptions.doSomething();
+		
+	}
+	
+	void doSomething() {
+		try {
+			throw new IOException();
+		} catch (IOException | IndexOutOfBoundsException e) {
+			System.out.println("more specific");
+		}
+		catch (Exception e) {
+			System.out.println("Caught ya!");
+			e.printStackTrace();
+		}
+		
+	}
+}
